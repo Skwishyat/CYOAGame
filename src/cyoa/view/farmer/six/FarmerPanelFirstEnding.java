31b238a00c52eaf718d736d2cd912431f01f021c
Kat <allilliams2003@gmail.com>
2022-05-03 09:23:14 -0600

    added javadoc for all the ending panels

--- src/cyoa/view/farmer/six/FarmerPanelFirstEnding.java
+++ src/cyoa/view/farmer/six/FarmerPanelFirstEnding.java
@@ -3,15 +3,53 @@ package cyoa.view.farmer.six;
 import javax.swing.JPanel;
 import cyoa.controller.Controller;
 
+/**
+ * This is the panel for the first possible ending 
+ * It is a good semi-common ending
+ * @author awil7254
+ *
+ */
 public class FarmerPanelFirstEnding extends JPanel
 {
 	private Controller app;
 	private JPanel panel;
 	
+	/**
+	 * Creates a new FarmerPanelFirstEnding with the controller it will be contained in
+	 * @param app The controller the panel will be contained in
+	 */
 	public FarmerPanelFirstEnding(Controller app)
 	{
 		super();
 		this.panel = new JPanel();
 		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	/**
+	 * sets up the panel, adding all the components to be displayed
+	 */
+	private void setupPanel()
+	{
+		
+	}
+	
+	/**
+	 * sets up the action listeners for all the buttons
+	 */
+	private void setupListeners()
+	{
+		
+	}
+	
+	/**
+	 * sets up the layout of where things are on the screen
+	 */
+	private void setupLayout()
+	{
+		
 	}
 }
--- src/cyoa/view/farmer/six/FarmerPanelFourthEnding.java
+++ src/cyoa/view/farmer/six/FarmerPanelFourthEnding.java
@@ -3,15 +3,53 @@ package cyoa.view.farmer.six;
 import javax.swing.JPanel;
 import cyoa.controller.Controller;
 
+/**
+ * This is the panel for the fouth possible ending
+ * It is a neutral rare ending
+ * @author awil7254
+ *
+ */
 public class FarmerPanelFourthEnding extends JPanel
 {
 	private JPanel panel;
 	private Controller app;
 	
+	/**
+	 * Creates a new FarmerPanelFourthEnding with the controller it will be contained in
+	 * @param app The controller the panel will be contained in
+	 */
 	public FarmerPanelFourthEnding(Controller app)
 	{
 		super();
 		this.panel = new JPanel();
 		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	/**
+	 * sets up the panel, adding all the components to be displayed
+	 */
+	private void setupPanel()
+	{
+		
+	}
+	
+	/**
+	 * sets up the action listeners for all the buttons
+	 */
+	private void setupListeners()
+	{
+		
+	}
+	
+	/**
+	 * sets up the layout of where things are on the screen
+	 */
+	private void setupLayout()
+	{
+		
 	}
 }

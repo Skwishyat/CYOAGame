1f2592ad29829118669b7e98ab3e1482ecd24d72
Kat <allilliams2003@gmail.com>
2022-05-03 09:45:50 -0600

    added javadoc for files in the second category

--- src/cyoa/view/farmer/two/FarmerPanelTwoOne.java
+++ src/cyoa/view/farmer/two/FarmerPanelTwoOne.java
@@ -3,15 +3,53 @@ package cyoa.view.farmer.two;
 import javax.swing.JPanel;
 import cyoa.controller.Controller;
 
+/**
+ * This is the panel for the first possibility of the second situation the user will encounter
+ * It has two options for continuing the story
+ * @author awil7254
+ *
+ */
 public class FarmerPanelTwoOne extends JPanel
 {
 	private JPanel panel;
 	private Controller app;
 	
+	/**
+	 * Creates a new FarmerPanelTwoOne with the controller it will be contained in
+	 * @param app The controller the panel will be contained in
+	 */
 	public FarmerPanelTwoOne(Controller app)
 	{
 		super();
 		this.panel = new JPanel();
 		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	/**
+	 * sets up the panel, adding all the components to be displayed
+	 */
+	private void setupPanel()
+	{
+		
+	}
+	
+	/**
+	 * sets up the action listeners for all the buttons
+	 */
+	private void setupListeners()
+	{
+		
+	}
+	
+	/**
+	 * sets up the layout of where things are on the screen
+	 */
+	private void setupLayout()
+	{
+		
 	}
 }

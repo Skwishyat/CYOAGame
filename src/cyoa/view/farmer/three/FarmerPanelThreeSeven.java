2b92c75d5a4d1b1d4e942fe0f1dae755a475d41d
Kat <allilliams2003@gmail.com>
2022-05-03 09:41:44 -0600

    added javadoc for all the files in the third category

--- src/cyoa/view/farmer/three/FarmerPanelThreeSeven.java
+++ src/cyoa/view/farmer/three/FarmerPanelThreeSeven.java
@@ -3,15 +3,53 @@ package cyoa.view.farmer.three;
 import javax.swing.JPanel;
 import cyoa.controller.Controller;
 
+/**
+ * This is the panel for the seventh possibility of the third situation the user will encounter
+ * It has three options for continuing the story
+ * @author awil7254
+ *
+ */
 public class FarmerPanelThreeSeven extends JPanel
 {
 	private JPanel panel;
 	private Controller app;
 	
+	/**
+	 * Creates a new FarmerPanelThreeSeven with the controller it will be contained in
+	 * @param app The controller the panel will be contained in
+	 */
 	public FarmerPanelThreeSeven(Controller app)
 	{
 		super();
 		this.panel = new JPanel();
 		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	/**
+	 * sets up the panel, adding all the components to be displayed
+	 */
+	private void setupPanel()
+	{
+		
+	}
+	
+	/**
+	 * sets up the action listeners for all the buttons
+	 */
+	private void setupListeners()
+	{
+		
+	}
+	
+	/**
+	 * sets up the layout of where things are on the screen
+	 */
+	private void setupLayout()
+	{
+		
 	}
 }
--- src/cyoa/view/farmer/three/FarmerPanelThreeSix.java
+++ src/cyoa/view/farmer/three/FarmerPanelThreeSix.java
@@ -3,16 +3,53 @@ package cyoa.view.farmer.three;
 import javax.swing.JPanel;
 import cyoa.controller.Controller;
 
-
+/**
+ * This is the panel for the sixth possibility of the third situation the user will encounter
+ * It has three options for continuing the story
+ * @author awil7254
+ *
+ */
 public class FarmerPanelThreeSix extends JPanel
 {
 	private JPanel panel;
 	private Controller app;
 	
+	/**
+	 * Creates a new FarmerPanelThreeSix with the controller it will be contained in
+	 * @param app The controller the panel will be contained in
+	 */
 	public FarmerPanelThreeSix(Controller app)
 	{
 		super();
 		this.panel = new JPanel();
 		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	/**
+	 * sets up the panel, adding all the components to be displayed
+	 */
+	private void setupPanel()
+	{
+		
+	}
+	
+	/**
+	 * sets up the action listeners for all the buttons
+	 */
+	private void setupListeners()
+	{
+		
+	}
+	
+	/**
+	 * sets up the layout of where things are on the screen
+	 */
+	private void setupLayout()
+	{
+		
 	}
 }
